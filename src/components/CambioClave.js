237da6519a20a5e3cfea668e4ad8d33ae5b1fb0e
robot2492 <rlopez.rjls@gmail.com>
2019-08-15 22:52:19 -0400

    Ruta de componente CambioClave

--- src/components/CambioClave.js
+++ src/components/CambioClave.js
@@ -2,22 +2,115 @@ import React, { Component } from 'react';
 import "./styles/CambioClave.css"
 
 class CambioClave extends Component {
-    render() {
-        return (
-            <div id="cambio-clave">
 
-                <h1>CAMBIAR CONTRASEÑA</h1>
-                
-                <div id="opciones">
+    state = {
+        stage: "main"
+    }
+
+    handleClick = (event) => {
+        const { name } = event.target;
+
+        this.setState({
+            stage: name
+        });
+    }
+
+    stages = () => {
+        switch(this.state.stage) {
+            case "main":
+                return(
+                    <div id="opciones">
 
                     <p>Introduzca el correo electronico con el que 
                     registro esta cuenta</p>
 
                     <input type="text" name="password"/>
 
-                    <button>ENVIAR</button>
+                    <button
+                        name="enviar"
+                        onClick={this.handleClick}
+                    >ENVIAR</button>
 
                 </div>
+                );
+            
+            case "enviar":
+                return(
+                    <div id="enviar">
+
+                        <div id="mensaje">
+                            <p>Hemos enviado un mensaje de 
+                            confirmación a la cuenta de  correo 
+                            que uso para registrarse, por favor 
+                            diríjase a su correo electrónico y 
+                            siga las instrucciones.</p>
+
+                            <p>Si no recibió el mensaje de click 
+                            en Reenviar</p>
+                        </div>
+
+                        <div id="botones">
+
+                            <button>REENVIAR</button>
+
+                            <button
+                                name="cambiar"
+                                onClick={this.handleClick}
+                            >ACEPTAR</button>
+
+                        </div>
+
+                    </div>
+                );
+
+            case "cambiar":
+                return(
+                    <div id="cambiar">
+
+                        <p>Ingrese la nueva contraseña</p>
+                        <input type="text"/>
+
+                        <p>Repita la contraseña</p>
+                        <input type="text"/>
+
+                        <button
+                            name="final"
+                            onClick={this.handleClick}
+                        >Enviar</button>
+                    </div>
+                );
+
+            case "final":
+                return(
+                    <div id="final">
+
+                        <p>
+                            Su cambio de contrase;a fue realizado 
+                            exitosamente, ingrese con su nueva
+                            contrase;a
+                        </p>
+
+                        <button>INGRESAR</button>
+                    </div>
+                );
+            default:
+                break;
+        }
+    }
+    render() {
+        return (
+            <div id="cambio-clave">
+
+                {
+                    (this.state.stage === "enviar") ?
+                        <h1>CONFIRMAR</h1>
+                    :
+                        <h1>CAMBIAR CONTRASEÑA</h1>
+                }
+                
+                {
+                    this.stages()
+                }
 
             </div>
         );
